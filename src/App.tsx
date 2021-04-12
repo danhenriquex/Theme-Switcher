0c4b8759bf793202e4146248bbded4b69a0aa6e5
danilohenrique <danilosantana@mat.ci.ufpb.br>
2021-04-12 17:35:52 -0300

    Finishing theme switcher from rocketseat

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import GlobalStyle from "./styles/global";
+import { ThemeProvider, DefaultTheme } from "styled-components";
+import light from "./styles/themes/light";
+import dark from "./styles/themes/dark";
+import usePersistedState from "./utils/usePersistedState";
 
 function App() {
+  const [theme, setTheme] = usePersistedState<DefaultTheme>("theme", light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === "light" ? dark : light);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <GlobalStyle />
+        <Header toggleTheme={toggleTheme} />
+      </div>
+    </ThemeProvider>
   );
 }
 
